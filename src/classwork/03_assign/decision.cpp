93d129d9c5d67ca54898595155580afd9091a4a8
BPitcher <53942483+BPitcher@users.noreply.github.com>
2020-11-04 05:46:12 -0600

    Homework 2 (Conditionals

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,57 @@
 //cpp
+#include "decision.h"
+
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string letter_grade;
+
+    if (grade >= 90 && grade <= 100)
+    {
+        letter_grade = "Your grade is an A!";
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        letter_grade = "Your grade is a B!";
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        letter_grade = "Your grade is a C!";
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        letter_grade = "Your grade is a D!";
+    }
+    else if (grade >= 0 && grade <= 59)
+    {
+        letter_grade = "Your grade is an F!";
+    }
+    return letter_grade;
+}
+
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string letter_grade;
+    int tens = grade / 10;
+    switch(tens)
+    {
+        case 10:
+            letter_grade = 'You got an A!';
+            break;
+        case 9:
+            letter_grade = 'You got an A!';
+            break;
+        case 8:
+            letter_grade = 'You got a B!';
+            break;
+        case 7:
+            letter_grade = 'You got a C!';
+            break;
+        case 6:
+            letter_grade = 'You got a D!';
+            break;
+        default:
+            letter_grade = 'You got an F!';
+            break;
+    }
+    return letter_grade;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,17 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
+#include <iostream>
 //Write namespace using statements for cout and cin
+using std::cout;
+using std::cin;
 
 int main() 
 {
-
+	int grade;
+	cout<<"Please enter your grade to convert into a letter: ";
+	cin>>grade;
+	get_letter_grade_using_if(grade);
+	get_letter_grade_using_switch(grade);
 	return 0;
 }
 
