79f4258c92e465b90f642738b619fb7a95ade251
BPitcher <53942483+BPitcher@users.noreply.github.com>
2020-11-04 05:48:20 -0600

    Homework 3

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,25 @@
 //cpp
+#include "loops.h"
+#include <iostream>
+using namespace std;
+
+int factorial(int num)
+{
+    int factorial;
+    int i;
+    do
+    {
+        factorial *=i;
+        cout<<"\nAt i = " + i;
+        cout<<"your factorial is: " + factorial;
+        i++;
+    } while (i<=num);
+
+    /*string choice;
+    cout<<"\nWould you like your factorial loop to keep going? (y\n)";
+    cin>>choice;
+    if(choice == "y")
+    {
+        num++;
+    }*/
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
-
+#include "loops.h"
+#include <iostream>
 //write using statements for cin and cout
-
+using std::cin;
+using std::cout;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,19 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	int num;
+	cout<<"Please enter a positive integer (1-10): ";
+	cin>>num;
+
+	do
+	{
+		cin>>num;
+		factorial(num);
+		cout<<"\nPlease enter a positive integer between 1-10 to keep going: ";
+		cin>>num;
+	} while (num < 1 && num > 10);
+	
+
 
 	return 0;
 }
